73066beb58d0cc55d597cfa4b2751fe07ac1e831
frankapplin@yahoo.com <frankapplin@yahoo.com>
2026-02-05 15:07:54 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,30 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @handle("button_1", "click")
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('pico_fn_c', 18) # Choose any number you like!
+
+  @handle("button_2", "click")
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('pico_fn_db', 18) # Choose any number you like!
+
+  @handle("button_3", "click")
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('pico_fn_d', 18) # Choose any number you like!
+
+  @handle("button_4", "click")
+  def button_4_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('pico_fn_eb', 18) # Choose any number you like!
+
+  @handle("button_5", "click")
+  def button_5_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('pico_fn_e', 18) # Choose any number you like!
+
+ 
